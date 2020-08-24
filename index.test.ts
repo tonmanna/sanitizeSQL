219435aa1e0594be7270b6d944bf3bd03692ae8d
Worawut Boontan <worawut@theiconweb.com>
2020-08-25 01:28:43 +0700

    sql injection prevention with all rules

--- index.test.ts
+++ index.test.ts
@@ -3,7 +3,8 @@ import { sanitizeSQL } from './index';
 // https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/SQL%20Injection/PostgreSQL%20Injection.md#postgresql-comments
 describe('Test sanitize function for each injection case from this link', () => {
     it('', () => {
-        const result = sanitizeSQL('SELECT * from UserName --/**/');
-        expect(result).toEqual('SELECT * from UserName ');
+        const result = sanitizeSQL('SELECT * from UserName --/**/;DROP TABLE USERNAME');
+        //TODO: syntax validate DROP TABLE should not run because is wrong syntax
+        expect(result).toEqual('SELECT * from UserName DROP TABLE USERNAME');
     });
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,32 @@
-const not_allow_text = ['--', '/\\*\\*/'];
+const not_allow_text = [
+    ';',
+    '--',
+    '/\\*\\*/',
+    'SELECT user',
+    'SELECT current_user',
+    'SELECT session_user',
+    'SELECT usename FROM pg_user',
+    'SELECT getpgusername()',
+    'SELECT usename FROM pg_user',
+    'SELECT usename FROM pg_user WHERE usesuper IS TRUE',
+    'SELECT usename, usecreatedb, usesuper, usecatupd FROM pg_user',
+    'SELECT current_database()',
+    'SELECT datname FROM pg_database',
+    'SELECT table_name FROM information_schema.tables',
+    "SELECT column_name FROM information_schema.columns WHERE table_name='data_table'",
+    "select query_to_xml('select * from pg_user',true,true,'')",
+    "select database_to_xml(true,true,'')",
+    "select database_to_xmlschema(true,true,'')",
+    "' and substr(version(),1,10) = 'PostgreSQL' and '",
+    "' and substr(version(),1,10) = 'PostgreXXX' and '",
+    'AND [RANDNUM]=(SELECT [RANDNUM] FROM PG_SLEEP([SLEEPTIME]))',
+    "select pg_ls_dir('./')",
+    "select pg_read_file('PG_VERSION', 0, 200)",
+    'CHR(65)',
+    'CHR(66)',
+    'CHR(67)',
+    '$$',
+];
 
 export const sanitizeSQL = (SQL: string): string => {
     not_allow_text.forEach((value) => {
