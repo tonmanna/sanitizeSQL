008117c2e9b93d646755add6e9b0f963a5c0f297
Worawut Boontan <worawut@theiconweb.com>
2022-08-31 21:16:23 +0700

    fix recurssion

--- index.js
+++ index.js
@@ -5,35 +5,57 @@ var not_allow_text = [
     ';',
     '--',
     '/\\*\\*/',
-    'SELECT user',
-    'SELECT current_user',
-    'SELECT session_user',
-    'SELECT usename FROM pg_user',
-    'SELECT getpgusername()',
-    'SELECT usename FROM pg_user',
-    'SELECT usename FROM pg_user WHERE usesuper IS TRUE',
-    'SELECT usename, usecreatedb, usesuper, usecatupd FROM pg_user',
-    'SELECT current_database()',
-    'SELECT datname FROM pg_database',
-    'SELECT table_name FROM information_schema.tables',
-    "SELECT column_name FROM information_schema.columns WHERE table_name='data_table'",
-    "select query_to_xml('select * from pg_user',true,true,'')",
-    "select database_to_xml(true,true,'')",
-    "select database_to_xmlschema(true,true,'')",
-    "' and substr(version(),1,10) = 'PostgreSQL' and '",
-    "' and substr(version(),1,10) = 'PostgreXXX' and '",
-    'AND [RANDNUM]=(SELECT [RANDNUM] FROM PG_SLEEP([SLEEPTIME]))',
-    "select pg_ls_dir('./')",
-    "select pg_read_file('PG_VERSION', 0, 200)",
+    'select user',
+    'current_user',
+    'session_user',
+    'pg_user',
+    /getpgusername/,
+    'usesuper',
+    'usecreatedb',
+    'usesuper',
+    'usecatupd',
+    /current_database\(\)/,
+    'pg_database',
+    'SELECTSELECT',
+    'FROM information_schema.tables',
+    "FROM information_schema.columns",
+    "WHERE table_name='data_table",
+    "select query_to_xml",
+    /database_to_xml/,
+    "database_to_xmlschema",
+    /'PostgreSQL' and /,
+    /'PostgreXXX' and /,
+    /AND \[RANDNUM\]=\(SELECT \[RANDNUM\] FROM PG_SLEEP\(\[SLEEPTIME\]\)\)/,
+    /FROM PG_SLEEP/,
+    /pg_ls_dir/,
+    /pg_read_file/,    
     'CHR(65)',
     'CHR(66)',
     'CHR(67)',
     '$$',
 ];
 exports.sanitizeSQL = function (SQL) {
+    if(SQL.length > 2147483648){
+        return "";
+    }   
+    function removeMultipleSpace(str){
+        return str.replace(/ +(?= )/g,'');
+    }     
     not_allow_text.forEach(function (value) {
         var reg = new RegExp(value, 'gi');
-        SQL = SQL.replace(reg, '').replace(/\s+/g, ' ');
+        let maxAttempt = 255
+        
+        let prevSQL = removeMultipleSpace(SQL).replace(reg, '').replace(/\s+/g, ' ');
+        for(let i = 0; i < maxAttempt; i++){
+           SQL = removeMultipleSpace(SQL).replace(reg, ' ').replace(/\s+/g, ' ');
+           if(prevSQL === SQL){
+               break;
+           }
+           prevSQL = SQL;
+        }
     });
+    if(SQL==" ")SQL = ''    
+    if(SQL.length>0)if(SQL[SQL.length-1] == ' ')SQL = SQL.slice(0, -1)
+    if(SQL.length>0)if(SQL[0] == ' ')SQL = SQL.slice(1, SQL.length);
     return SQL;
 };
--- index.test.ts
+++ index.test.ts
@@ -7,4 +7,50 @@ describe('Test sanitize function for each injection case from this link', () => 
         //TODO: syntax validate DROP TABLE should not run because is wrong syntax
         expect(result).toEqual('SELECT * from UserName DROP TABLE USERNAME');
     });
+    it('', () => {
+        const result = sanitizeSQL(
+            'SELECTSELECT current_database()current_database()SELECTSELECT current_database()current_database()SELECTSELECT current_database()current_database()SELECTSELECT current_database()current_database()SELECTSELECT current_database()current_database()',
+        );
+        //TODO: syntax validate DROP TABLE should not run because is wrong syntax
+        expect(result).toEqual('');
+    });
+    it('', () => {
+        const result = sanitizeSQL("'PostgreSQL' and ");
+        //TODO: syntax validate DROP TABLE should not run because is wrong syntax
+        expect(result).toEqual('');
+    });
+    it('', () => {
+        const mock = `    SELECT
+            id,
+            page_id "pageID",
+            seller_id "sellerID",
+            name "name",
+            picture,
+            url,
+            access_token "accessToken",
+            CASE 
+              WHEN page_type = 'lazada' THEN to_char(updated_at + (payload::json->>'expires_in')::NUMERIC * INTERVAL '1 second', 'YYYY-MM-DD HH24:MM:SS') 
+              WHEN page_type = 'shopee' THEN to_char(updated_at + (payload::json->>'expire_in') :: NUMERIC * INTERVAL '1 second', 'YYYY-MM-DD HH24:MM:SS') 
+              END AS "accessTokenExpire",
+            page_type "pageType",
+              payload::json->>'refresh_token' "refreshToken",
+            CASE 
+              WHEN page_type = 'lazada' THEN to_char(updated_at + (payload::json->>'refresh_expires_in')::NUMERIC * INTERVAL '1 second', 'YYYY-MM-DD HH24:MM:SS') 
+              WHEN page_type = 'shopee' THEN to_char(updated_at + INTERVAL '1' DAY * 1231 , 'YYYY-MM-DD HH24:MM:SS') 
+              END AS "refreshTokenExpire",
+            created_at AS "createdAt",
+            updated_at AS "updatedAt",
+            payload
+    FROM
+            pages_third_party
+    WHERE
+            active = TRUE
+            AND page_type = ANY($1)
+            AND page_id = $2`;
+        const result = sanitizeSQL(mock);
+        //TODO: syntax validate DROP TABLE should not run because is wrong syntax
+        expect(result).toEqual(
+            `SELECT id, page_id "pageID", seller_id "sellerID", name "name", picture, url, access_token "accessToken", CASE WHEN page_type = 'lazada' THEN to_char(updated_at + (payload::json->>'expires_in')::NUMERIC * INTERVAL '1 second', 'YYYY-MM-DD HH24:MM:SS') WHEN page_type = 'shopee' THEN to_char(updated_at + (payload::json->>'expire_in') :: NUMERIC * INTERVAL '1 second', 'YYYY-MM-DD HH24:MM:SS') END AS "accessTokenExpire", page_type "pageType", payload::json->>'refresh_token' "refreshToken", CASE WHEN page_type = 'lazada' THEN to_char(updated_at + (payload::json->>'refresh_expires_in')::NUMERIC * INTERVAL '1 second', 'YYYY-MM-DD HH24:MM:SS') WHEN page_type = 'shopee' THEN to_char(updated_at + INTERVAL '1' DAY * 1231 , 'YYYY-MM-DD HH24:MM:SS') END AS "refreshTokenExpire", created_at AS "createdAt", updated_at AS "updatedAt", payload FROM pages_third_party WHERE active = TRUE AND page_type = ANY($1) AND page_id = $2`,
+        );
+    });
 });
